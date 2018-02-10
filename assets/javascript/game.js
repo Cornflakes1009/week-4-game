15bd45c576d13d7727b8f4e2f09dd09ae3d1f1b9
Harold Davidson <harold.davidson89@gmail.com>
2018-02-09 23:40:14 -0600

    crystal random number set

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,54 @@
 $(document).ready(function () {
-    // sets random value for crystals 1-10
+    
+    
+    // function to set random numbers between 1-9 to assign to each gem
     var crystalValueSelector;
     var randomNumber = function(){
         crystalValueSelector = Math.floor(Math.random() * 10);
         return crystalValueSelector;
     }
+    var totalScore = 0;
     
     // sets random value for number to guess 25-50
     var numberToAchieve = Math.floor(Math.random() * (50 - 25) + 25);
+
     // setting each crystal to have a random number value when the game loads
-    var gem1 = randomNumber();
-    var gem2 = randomNumber();
-    var gem3 = randomNumber();
-    var gem4 = randomNumber();
-    console.log(gem1, gem2, gem3, gem4);
+    var gem1Value = 1;
+    var gem2Value = randomNumber();
+    var gem3Value = randomNumber();
+    var gem4Value = randomNumber();
+    
     // updates the number to mach field with the random number
     $('.numberToMatch').html(numberToAchieve);
 
+    if(totalScore === numberToAchieve) {
+        alert("winner");
+    }
+
+
     $('.gemButton1').on('click', function () {
         console.log("gem1 was clicked");
+        totalScore += gem1Value;
+        $('.score').html(totalScore);
+
+    })
+    $('.gemButton2').on('click', function () {
+        console.log("gem2 was clicked");
+        totalScore += gem2Value;
+        $('.score').html(totalScore);
+
+    })
+    $('.gemButton3').on('click', function () {
+        console.log("gem3 was clicked");
+        totalScore += gem3Value;
+        $('.score').html(totalScore);
+
+    })
+    $('.gemButton4').on('click', function () {
+        console.log("gem4 was clicked");
+        totalScore += gem4Value;
+        $('.score').html(totalScore);
+
     })
 
 
